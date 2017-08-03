ce2af65d41e2d6428d6d29f6dca8c6878e82d0d0
Lift-lrcover <873752961@qq.com>
2017-08-03 10:56:54 +0800

    Update Config.java

--- app/src/main/java/com/common/Config.java
+++ app/src/main/java/com/common/Config.java
@@ -4,7 +4,7 @@ package com.common;
  * Created by qingliang on 16/10/31.
  */
 public class Config {
-    public static final String APP_ID = "10091852";                                 // 替换APP_ID
-    public static final String SECRET_ID = "AKIDHUZXYYAKKEzc6OARKG5oPsgKn9MPAvPm";  // 替换SECRET_ID
-    public static final String SECRET_KEY = "Y2RjMMH4oP9qQp865vlW6UmjaGpCNMzS";     // 替换SECRET_KEY
+    public static final String APP_ID = "<your APP_ID>";                                 // 替换APP_ID
+    public static final String SECRET_ID = "<your SECRET_ID>";  // 替换SECRET_ID
+    public static final String SECRET_KEY = "your SECRET_KEY";     // 替换SECRET_KEY
 }
